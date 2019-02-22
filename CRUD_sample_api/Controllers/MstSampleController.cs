33a3347727f986c73c1db3d9c0b1bb0dd5ebcfb5
ArneilPaulPolican <policanarneilpaul@gmail.com>
2019-02-22 10:48:35 +0800

    February 2019-02-22

---
+++ CRUD_sample_api/Controllers/MstSampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+using CRUD_sample_api.Models;
+//using Microsoft.AspNet.Identity;
+
+namespace CRUD_sample_api.Controllers
+{
+    [RoutePrefix("api/Sample")]
+    public class MstSampleController : ApiController
+    {
+        private Data.SampleDataClassDataContext db = new Data.SampleDataClassDataContext();
+        //List
+        [HttpGet, Route("List")]
+        public List<Models.TableOne> GetTableData()
+        {
+            var TableList = from d in db.Table1s
+                            select new Models.TableOne
+                            {
+                                Id = d.Id,
+                                Description = d.Description,
+                                Remarks = d.Remarks
+                            };
+            return TableList.ToList();
+        }
+
+    }
+}
---
+++ CRUD_sample_api/Models/TableOne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_sample_api.Models
+{
+    public class TableOne
+    {
+        public Int32 Id { get; set; }
+        public String Description { get; set; }
+        public String Remarks { get; set; }
+    }
+}
